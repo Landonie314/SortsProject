37518654e755a56b4ab8f1d224e0b01f8b6a9cfe
Landonie314 <trumpet.landon@gmail.com>
2022-10-07 14:47:44 -0500

    Removed Heap sort added Merge Sort
    Removed heap sort method and heapify and added merge sort

--- AlgoSorts.py
+++ AlgoSorts.py
@@ -104,40 +104,47 @@ def radixSort(array):
         exp *= 10
     return arr
         
-        #used for heap sort
-def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1  # left = 2*i + 1
-    r = 2 * i + 2  # right = 2*i + 2
- # See if left child of root exists and is
- # greater than root
-    if l < n and arr[i] < arr[l]:
-        largest = l
- # See if right child of root exists and is
- # greater than root
-    if r < n and arr[largest] < arr[r]:
-        largest = r
- # Change root, if needed
-    if largest != i:
-        (arr[i], arr[largest]) = (arr[largest], arr[i])  # swap
-  # Heapify the root.
-        heapify(arr, n, largest)
-# The main function to sort an array of given size
+#merge sort
+def mergeSort(arr):
+    if len(arr) > 1:
  
-def heapSort(array):
-    arr = list(array)
-    n = len(arr)
- # Build a maxheap.
- # Since last parent will be at ((n//2)-1) we can start at that location.
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
- # One by one extract elements
-    for i in range(n - 1, 0, -1):
-        (arr[i], arr[0]) = (arr[0], arr[i])  # swap
-        heapify(arr, i, 0)
-    return arr
-        
-
+         # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        mergeSort(L)
+ 
+        # Sorting the second half
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
 
 #generate the arrays
 def genArray(count,floor=RAND_FLOOR,ceil=RAND_CEIL):
@@ -191,8 +198,8 @@ sorts = [
     'average': arrays_avg
     },
     {
-    'name': 'Heap Sort',
-    'func': heapSort,
+    'name': 'Merge Sort',
+    'func': mergeSort,
     'best': arrays_identical,
     'worst': arrays_distinct,
     'average': arrays_avg
